f0a34e36a0aac952ad86e555b9e53626a67b3cc5
targoweb222 <138272002+targoweb222@users.noreply.github.com>
2023-08-27 11:20:17 +0300

    Add files via upload

---
+++ execute.bat
@@ -0,0 +1,20 @@
+```
+cd %UserProfile%/Desktop
+mkdir WEBSITE
+cd WEBSITE
+echo <!DOCTYPE html> >> index.html
+echo <html lang="en"> >> index.html
+echo <head> >> index.html
+echo <meta charset="UTF-8"> >> index.html
+echo <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> >> index.html
+echo <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> >> index.html
+echo </head> >> index.html
+echo <body> >> index.html
+echo <div class="container"> >> index.html
+echo <h1>Hello, World!</h1> >> index.html
+echo </div> >> index.html
+echo <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> >> index.html
+echo <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> >> index.html
+echo </body> >> index.html
+echo </html> >> index.html
+```
